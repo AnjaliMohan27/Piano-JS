ada77dabe32cf48a64879eb1964ca41f822cbe01
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-06-01 10:55:01 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,159 @@
+const white1=document.querySelector(".key1");
+white1.addEventListener("click",()=> {
+  const sound1=document.querySelector(".note1");
+  sound1.play();
+});
+
+const black1=document.querySelector(".key2");
+black1.addEventListener("click",()=> {
+  const sound2=document.querySelector(".note2");
+  sound2.play();
+});
+
+const white2=document.querySelector(".key3");
+white2.addEventListener("click",()=> {
+  const sound3=document.querySelector(".note3");
+  sound3.play();
+});
+
+const black2=document.querySelector(".key4");
+black2.addEventListener("click",()=> {
+  const sound4=document.querySelector(".note4");
+  sound4.play();
+});
+
+
+const white3=document.querySelector(".key5");
+white3.addEventListener("click",()=> {
+  const sound5=document.querySelector(".note5");
+  sound5.play();
+});
+
+
+const white4=document.querySelector(".key6");
+white4.addEventListener("click",()=> {
+  const sound6=document.querySelector(".note6");
+  sound6.play();
+});
+
+
+const black3=document.querySelector(".key7");
+black3.addEventListener("click",()=> {
+  const sound7=document.querySelector(".note7");
+  sound7.play();
+});
+
+
+const white5=document.querySelector(".key8");
+white5.addEventListener("click",()=> {
+  const sound8=document.querySelector(".note8");
+  sound8.play();
+});
+
+
+const black4=document.querySelector(".key9");
+black4.addEventListener("click",()=> {
+  const sound9=document.querySelector(".note9");
+  sound9.play();
+});
+
+
+const white6=document.querySelector(".key10");
+white6.addEventListener("click",()=> {
+  const sound10=document.querySelector(".note10");
+  sound10.play();
+});
+
+const black5=document.querySelector(".key11");
+black5.addEventListener("click",()=> {
+  const sound11=document.querySelector(".note11");
+  sound11.play();
+});
+
+
+const white7=document.querySelector(".key12");
+white7.addEventListener("click",()=> {
+  const sound12=document.querySelector(".note12");
+  sound12.play();
+});
+
+
+
+const white8=document.querySelector(".key13");
+white8.addEventListener("click",()=> {
+  const sound13=document.querySelector(".note13");
+  sound13.play();
+});
+
+const black6=document.querySelector(".key14");
+black6.addEventListener("click",()=> {
+  const sound14=document.querySelector(".note14");
+  sound14.play();
+});
+
+const white9=document.querySelector(".key15");
+white9.addEventListener("click",()=> {
+  const sound15=document.querySelector(".note15");
+  sound15.play();
+});
+
+const black7=document.querySelector(".key16");
+black7.addEventListener("click",()=> {
+  const sound16=document.querySelector(".note16");
+  sound16.play();
+});
+
+
+const white10=document.querySelector(".key17");
+white10.addEventListener("click",()=> {
+  const sound17=document.querySelector(".note17");
+  sound17.play();
+});
+
+
+const white11=document.querySelector(".key18");
+white11.addEventListener("click",()=> {
+  const sound18=document.querySelector(".note18");
+  sound18.play();
+});
+
+
+const black8=document.querySelector(".key19");
+black8.addEventListener("click",()=> {
+  const sound19=document.querySelector(".note19");
+  sound19.play();
+});
+
+
+const white12=document.querySelector(".key20");
+white12.addEventListener("click",()=> {
+  const sound20=document.querySelector(".note20");
+  sound20.play();
+});
+
+
+const black9=document.querySelector(".key21");
+black9.addEventListener("click",()=> {
+  const sound21=document.querySelector(".note21");
+  sound21.play();
+});
+
+
+const white13=document.querySelector(".key22");
+white13.addEventListener("click",()=> {
+  const sound22=document.querySelector(".note22");
+  sound22.play();
+});
+
+const black10=document.querySelector(".key23");
+black10.addEventListener("click",()=> {
+  const sound23=document.querySelector(".note23");
+  sound23.play();
+});
+
+
+const white14=document.querySelector(".key24");
+white14.addEventListener("click",()=> {
+  const sound24=document.querySelector(".note24");
+  sound24.play();
+});
